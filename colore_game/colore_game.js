47a199f546ea5c3a71f09b206515008bcbf2c136
iamayush25 <ayush22@navgurukul.org>
2023-11-07 11:35:21 +0530

    added new projecct

---
+++ colore_game/colore_game.js
@@ -0,0 +1,58 @@
+let box1 = true;
+document.getElementById('circle1').addEventListener('click', () => {
+    if (box1) {
+        document.getElementById('inner_container').style.background = 'aqua'
+        
+
+    }
+})
+
+let box2 = true;
+document.getElementById('circle2').addEventListener('click', () => {
+    if (box2) {
+        document.getElementById('inner_container').style.background = 'red'
+        
+
+    }
+})
+let box3 = true;
+document.getElementById('circle3').addEventListener('click', () => {
+    if (box3) {
+        document.getElementById('inner_container').style.background = 'greenyellow';
+        
+
+    }
+})
+let box4 = true;
+document.getElementById('circle4').addEventListener('click', () => {
+    if (box4) {
+        document.getElementById('inner_container').style.background = 'blueviolet'
+        
+
+    }
+})
+let box5 = true;
+document.getElementById('circle5').addEventListener('click', () => {
+    if (box5) {
+        document.getElementById('inner_container').style.background = 'purple'
+        
+
+    }
+})
+let box6 = true;
+document.getElementById('circle6').addEventListener('click', () => {
+    if (box6) {
+        document.getElementById('inner_container').style.background = 'chartreuse'
+        
+
+    }
+})
+
+let innerDiv = true;
+document.getElementById('reset').addEventListener('click', () => {
+    if (innerDiv) {
+        document.getElementById('inner_container').style.background = 'wheat'
+        
+
+    }
+})
